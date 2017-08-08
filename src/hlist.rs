4d659e106bcac64df12f91a3b27010fee1530b66
Andrei Oprisan <oprisan@eloquentix.com>
2017-08-08 12:42:01 +0300

    enum based hlist

--- src/hlist.rs
+++ src/hlist.rs
@@ -36,32 +36,46 @@ fn test_prepend(){
     assert_eq!(list.tail.tail, HNil);
 }
 
-
-enum EHList<T,V> {
+#[derive(Debug, PartialEq)]
+pub enum EHList<T,V> {
     Nil,
     Cons(T,V)
 }
 
+pub fn nil() -> EHList<(),()> {
+    EHList::Nil
+}
+
+pub fn cons<T,V>(t:T,v: V) -> EHList<T,V> {
+    EHList::Cons(t,v)
+}
+
 impl<T,V> EHList<T,V> {
-    pub fn nil() -> EHList<T,V> {
-        EHList::Nil
-    }
-
-    pub fn cons(t:T,v: V) -> EHList<T,V> {
-        EHList::Cons(t,v)
-    }
-
     pub fn prepend<N>(self, n: N) -> EHList<N, Self> {
-        EHList::cons(n, self)
+        EHList::Cons(n, self)
+    }
+
+    pub fn head(&self) -> Option<&T> {
+        match self {
+            &EHList::Cons(ref h, _) => Some(h),
+            &EHList::Nil => None
+        }
+    }
+
+    pub fn tail(&self) -> Option<&V> {
+        match self {
+            &EHList::Cons(_, ref rest) => Some(rest),
+            &EHList::Nil => None
+        }
     }
 
 }
 
-//#[test]
-//fn test_prepend_enum(){
-//    let list = EHList::nil().prepend(100).prepend("string".to_string());
-//    let EHList::Cons(str, rest) = list;
-//    assert_eq!(str, "string".to_string());
-//    //assert_eq!(list.1.0, 100);
-//    //assert_eq!(list.tail.tail, HNil);
-//}
+#[test]
+fn test_prepend_enum(){
+    let list = nil().prepend(100).prepend("string".to_string());
+    assert_eq!(list.head(), Some("string".to_string()).as_ref());
+    assert_eq!(list.tail().unwrap().head(), Some(100).as_ref());
+    assert_eq!(list.tail().unwrap().tail().unwrap(), &nil());
+
+}
